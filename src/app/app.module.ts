15daa819595db923c308465b0c2a288fd250a92f
i-timur <i.timur@mail.ru>
2022-07-10 12:04:31 +0300

    just testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaneDirective } from './directives/pane.directive';
+import { ViewChildComponent } from './components/view-child/view-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaneDirective,
+    ViewChildComponent
   ],
   imports: [
     BrowserModule
